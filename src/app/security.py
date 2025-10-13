eca55a34461138746dde2be1bcb92d40a811f04f
Dolce-Vitah <vadenisenko@edu.hse.ru>
2025-10-13 03:15:30 +0300

    fix(src/app): add int convertion

--- src/app/security.py
+++ src/app/security.py
@@ -14,7 +14,7 @@ from .utils.token_utils import is_token_revoked
 
 SECRET_KEY = os.getenv("SECRET_KEY", "wishlist-secret-key")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60)
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
